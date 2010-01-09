2e420820d3b3fb228c810937539f95a618a2c271
Eric Wong <normalperson@yhbt.net>
2010-01-09 22:52:27 +0000

    posix_mq 0.3.0
    This release adds a few new API methods, fixes MRI 1.8.6
    support.  We should now have full feature parity with
    underlying POSIX message queue C API.
    
    * POSIX_MQ#notify(&block)
      RDoc: http://bogomips.org/ruby_posix_mq/POSIX_MQ.html#M000001
      This is only supported on platforms that implement
      SIGEV_THREAD with mq_notify(3) (tested with glibc + Linux).
      Other platforms will have to continue to rely on signal
      notifications via POSIX#notify=signal, or IO notifications
      in FreeBSD (and Linux).
    
    * POSIX_MQ#shift([buffer [,timeout]])
      Shorthand for the common "POSIX_MQ#receive.first"
      when you do not care for priority of the received message.
    
    Rev, EventMachine and Reactor support are planned for
    Linux, FreeBSD and possibly any other platforms where POSIX
    message queues are implemented with a file descriptor.

--- lib/posix_mq.rb
+++ lib/posix_mq.rb
@@ -1,8 +1,8 @@
 # -*- encoding: binary -*-
 class POSIX_MQ
 
-  # version of POSIX_MQ, currently 0.1.0
-  VERSION = '0.2.0'
+  # version of POSIX_MQ, currently 0.3.0
+  VERSION = '0.3.0'
 
   # An analogous Struct to "struct mq_attr" in C.
   # This may be used in arguments for POSIX_MQ.new and
