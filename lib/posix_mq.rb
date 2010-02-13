05e577616b74bea99a0e43e07f28823ddda1aaf9
Eric Wong <normalperson@yhbt.net>
2010-02-13 03:39:17 -0800

    posix_mq 0.3.1
    This fixes a misuse of the Ruby API leading to memory leaks in
    cases where message queues are continually opened and closed
    throughout the lifetime of the application.
    
    Fortunately applications have little reason to repeatedly open
    and close message queue descriptors: they are
    multi-thread/multi-process-safe in every way imaginable and also
    capable of non-blocking operation.

--- lib/posix_mq.rb
+++ lib/posix_mq.rb
@@ -1,8 +1,8 @@
 # -*- encoding: binary -*-
 class POSIX_MQ
 
-  # version of POSIX_MQ, currently 0.3.0
-  VERSION = '0.3.0'
+  # version of POSIX_MQ, currently 0.3.1
+  VERSION = '0.3.1'
 
   # An analogous Struct to "struct mq_attr" in C.
   # This may be used in arguments for POSIX_MQ.new and
