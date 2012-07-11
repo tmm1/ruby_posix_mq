abbe5f16a9c3e17a9a6bab39bb105dc655e55eb6
ew@fbsd90-32.(none) <ew@fbsd90-32.(none)>
2012-07-11 02:25:44 +0000

    extconf: fix __mq_oshandle() detection on FreeBSD
    We need to have -lrt in LDFLAGS before we can detect it.

--- ext/posix_mq/extconf.rb
+++ ext/posix_mq/extconf.rb
@@ -3,7 +3,6 @@ require "mkmf"
 have_header("sys/select.h")
 have_header("signal.h")
 have_header("mqueue.h") or abort "mqueue.h header missing"
-have_func("__mq_oshandle")
 have_header("pthread.h")
 have_func("rb_str_set_len")
 have_func('rb_thread_blocking_region')
@@ -11,6 +10,7 @@ have_library("m")
 have_library("rt")
 have_library("pthread")
 
+have_func("__mq_oshandle")
 have_func("mq_timedsend")
 have_func("mq_timedreceive")
 create_makefile("posix_mq_ext")
